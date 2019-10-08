bec163b2c745dca9ba94df0a7d445f23c1f2c42d
Mesqueeb <mesqueeb@gmail.com>
2019-10-08 12:44:30 +0900

    add infobox wrapper component

--- ui/dev/src/boot/vueComponents.js
+++ ui/dev/src/boot/vueComponents.js
@@ -1,9 +1,11 @@
 import InfoCard from '../components/InfoCard'
 import SourceTab from '../components/SourceTab'
 import PropTabsPanel from '../components/PropTabsPanel'
+import InfoBoxWrapper from '../components/InfoBoxWrapper'
 
 export default ({ Vue }) => {
   Vue.component(InfoCard.name, InfoCard)
   Vue.component(SourceTab.name, SourceTab)
   Vue.component(PropTabsPanel.name, PropTabsPanel)
+  Vue.component(InfoBoxWrapper.name, InfoBoxWrapper)
 }
---
+++ ui/dev/src/components/InfoBoxWrapper.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="info-box-wrapper">
+    <q-badge :color="color">{{ label }}</q-badge>
+    <div class="_inner-wrapper">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style lang="sass">
+// $
+
+.info-box-wrapper
+  > ._inner-wrapper
+    border: thin solid $primary
+    min-height: 70px
+    min-width: 170px
+    margin-top: -10px
+    padding: 1em
+    // display: flex
+    > *
+      width: 100%
+      height: 100%
+      // justify-self: stretch
+  // if markdown is used in slot:
+  .q-markdown--token
+    white-space: pre-line
+    word-break: break-word
+
+</style>
+
+<script>
+import { pascalCase } from 'case-anything'
+
+export default {
+  name: pascalCase('info-box-wrapper'),
+  props: {
+    color: String,
+    label: String,
+  },
+  computed: {},
+  methods: {},
+}
+</script>
